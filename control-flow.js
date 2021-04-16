6e0487db726aaedc5961e7205c0cdcbc3fe1e902
infomasudcse <infomasud@gmail.com>
2021-04-17 01:31:28 +0600

    if elseif else

--- control-flow.js
+++ control-flow.js
@@ -1,21 +1,21 @@
 //while loop 
 
-const names = ['kalam', 'akash', 'azim'];
+//const names = ['kalam', 'akash', 'azim'];
 
-let i = 0;
+//let i = 0;
 // while(i < 5){
 //     console.log('in loop: ', i);
 //     i++;
 // }
 
-while(i < names.length){
-    console.log(names[i]);
-    i++;
-}
+//while(i < names.length){
+   // console.log(names[i]);
+  //  i++;
+//}
 
 //do while 
 
-let j = 5;
-do{
-    console.log(j);
-}while(j < 5);
+//let j = 5;
+//do{
+   // console.log(j);
+//}while(j < 5);
---
+++ if-statement.js
@@ -0,0 +1,24 @@
+//if statements
+
+// const age = 25;
+
+// if(age > 20){
+//     console.log('You are over 20 years old');
+// }
+
+// const names = ['abul', 'babul', 'choto', 'dulal'];
+// if(names.length < 4){
+//     console.log("that's a lot of people");
+// }
+
+const password = 'pass';
+
+if(password.length >= 12){
+    console.log(' that password is might strong ! ');
+
+}else if(password.length >= 8){
+    console.log(' that password is long enough ! ');
+
+}else{
+    console.log('Please use more longer password at least 8 character ');
+}
