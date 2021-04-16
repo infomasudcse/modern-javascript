561221df582faa30fd6a2b0a6d30a5bea7ecde2c
infomasudcse <infomasud@gmail.com>
2021-04-17 02:05:57 +0600

    data-array

--- main.js
+++ main.js
@@ -1,52 +1,26 @@
-// Math operators are  +,  -, * , / , **, % 
-let radius = 5; 
-const pi  = 3.14;
-console.log(10/2);
 
-let result = radius % 3; // The % operator returns the remainder after division task performed 
-let area = pi * radius**2;
-// Order of operation in Math calculation 
-// B -- Brackets 
-// I – Indices 
-// D – Division 
-// M – Multiplication 
-// A – Addition 
-// S – Subtraction 
+//array 
 
-let result = 5 * (25 -5)**3 ;
+let friends = [‘Rahim’, ‘Karim’,’Azim’];
+console.log(friends);
+
+friends[1] = ‘Abdul’;
+console.log(friends[1]);
+
+
+let random = [‘Badshah’,’Abdul’, 20, 50];
+console.log(random);
+
+
+console.log(friends.length);
+//let result = friends.join(‘ , ’);
+console.log(result);
+//let result = friends.indexOf(‘Azim’);
+//let result = friends.concat([‘Salah’, ’Aman’]);
+let result = friends.push(‘Kalam’);
+result = friends.pop();
 console.log(result);
 
-//sorthand
-let likes = 10;
-
-// likes = likes + 1 ;
-// likes++;  
-// console.log(likes);
-
-// likes--;  
-// console.log(likes);
-
-
-// likes += 10;  
-// console.log(likes);
-
-
-likes -=5; // console.log(likes);
-likes *= 2; // console.log(likes);
-
-// NaN = Not a number 
-// console.log(  5/ ‘hi’); // output NaN
-//Concatenation string and number 
-console.log(‘You have ’+ likes + ‘ likes on your post’);
-//Template string way 
-const title = ‘Best read of 2020’;
-const author =’Robert’;
-const likes = 30;
-let result = `This blog called ${title} by ${author} has ${likes} likes`;
-console.log(result);
-// creating HTML template
-let html = `<h2>${title}</h2><p>${author}</p>`;
-console.log(html);
 
 
 
