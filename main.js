c77efebba8334b36fde2c33c4f942b7a047e2156
infomasudcse <infomasud@gmail.com>
2021-04-17 02:45:10 +0600

    switch statement

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-// break 
+//switch statement 
+// it compares  as ===
 
-const scores = [50, 25, 0, 100, 20, 10];
-for(let i = 0; i < scores.length; i++){
+const grade = 'D';
+
+switch(grade){
+    case 'A':
+        console.log('You got an A!');
+        break;
+    case 'B':
+        console.log('You got an B!');
+        break;
+    case 'C':
+        console.log('You got an C!');
+        break;
+    case 'D':
+        console.log('You got an D!');
+        break;
+    case 'E':
+        console.log('You got an E!');
+        break;
+    case 'F':
+        console.log('You got an F!');
+        break;
+    default:
+        console.log('Not a valid grade');
 
 
-    if(scores[i] === 0){
-        continue; // this statement finish current iteration and do not let execute  rest of code for this iteration but loop continue for rest of iteration 
-    }
-
-
-    console.log('Your score: ', scores[i]);
-
-
-
-
-    if(scores[i] === 100){
-        console.log('congrets, You got the  top score');
-        break; // this break the loop and control flow gets out form the loop 
-    }
 }
+
