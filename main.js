4e11dd36d9ca25c08be6dfdc8d30ae702315c196
infomasudcse <infomasud@gmail.com>
2021-04-17 02:12:28 +0600

    other data types

--- main.js
+++ main.js
@@ -1,26 +1,68 @@
 
-//array 
+// Undefined
+let year ;
+console.log(year, year + 2, `The year is ${year}`);
+// Null
+let myAge = null; 
+console.log(myAge, myAge + 2, `The year is ${myAge}`);
 
-let friends = [‘Rahim’, ‘Karim’,’Azim’];
-console.log(friends);
+// Boolean
+console.log(true);
+console.log(flase);
 
-friends[1] = ‘Abdul’;
-console.log(friends[1]);
-
-
-let random = [‘Badshah’,’Abdul’, 20, 50];
-console.log(random);
-
-
-console.log(friends.length);
-//let result = friends.join(‘ , ’);
-console.log(result);
-//let result = friends.indexOf(‘Azim’);
-//let result = friends.concat([‘Salah’, ’Aman’]);
-let result = friends.push(‘Kalam’);
-result = friends.pop();
+//Methods can return boolan
+let email = ‘masud@infomasud.com’;
+let result = email.includes(‘@’);
 console.log(result);
 
+let names = [‘Karim’,’Masud’,’Alam’];
+let results = names.includes(‘Masud’);
+console.log(results);
+// comparison operators 
+let age = 30;
+console.log(age == 30);
+console.log(age == 25);
+console.log(age != 30);
+console.log(age != 25);
+console.log(age > 25);
+console.log(age  < 25);
+console.log(age  <= 30);
+console.log(age >= 30);
+
+let name = ‘karim’;
+console.log(name  ==  ‘karim’);
+console.log(name  ==  ‘Karim’);
+console.log(name  > ‘abdul’);
+console.log(name  >  ‘Alam’);
 
 
+//Comparison
+
+//loose comparison
+let age = 30;
+console.log(age == 30);
+console.log(age == ‘30’);
+console.log(age != 30);
+console.log(age != ‘30’);
+
+
+// strict comparison
+console.log(age === 30);
+console.log(age === ‘30’);
+console.log(age !== 30);
+console.log(age !== ‘30’);
+
+
+// Type Conversion
+
+let score =  ‘100’;
+console.log(score + 1);
+score = Number(score);
+console.log(score + 1)
+console.log( typeof score); //get variable type
+
+
+let agee = 30;
+agee = String(30);
+console.log(agee, typeof agee);
 
