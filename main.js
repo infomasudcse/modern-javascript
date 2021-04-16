d58a9569df3f1d0fd09cded337077e8e041663d4
infomasudcse <infomasud@gmail.com>
2021-04-17 02:01:36 +0600

    Math operation

--- main.js
+++ main.js
@@ -1,31 +1,52 @@
-//console.log('Hello Javascript');
-//String 
-let email = ‘infomasud@gmail.com’;
-console.log(email);
-// Concatenation 
-let firstName = ‘’;
-let lastName = ‘’;
-let fullName = fistName+ ‘ ’ +lastName;
-console.log(fullName);
+// Math operators are  +,  -, * , / , **, % 
+let radius = 5; 
+const pi  = 3.14;
+console.log(10/2);
 
-//Getting characters
-console.log(fullName[2]);
+let result = radius % 3; // The % operator returns the remainder after division task performed 
+let area = pi * radius**2;
+// Order of operation in Math calculation 
+// B -- Brackets 
+// I – Indices 
+// D – Division 
+// M – Multiplication 
+// A – Addition 
+// S – Subtraction 
+
+let result = 5 * (25 -5)**3 ;
+console.log(result);
+
+//sorthand
+let likes = 10;
+
+// likes = likes + 1 ;
+// likes++;  
+// console.log(likes);
+
+// likes--;  
+// console.log(likes);
 
 
-//String Length
-console.log(fullName.length);
-
-//Method
-console.log(fullName.toUpperCase());
-console.log(fullName.toLowerCase());
+// likes += 10;  
+// console.log(likes);
 
 
-let index = email.indexOf(‘@’);
-let indexOfN = email.lastIndexOf(‘n’);
-let result = email.slice(2,5); //  .slice(indexNumberWhereToStart, IndexNumberWhereToStop) 
-let substring = email.substr(5,10); //  .substr(indexNumberFromWhereStart, HowMuchCharacterToTake)
+likes -=5; // console.log(likes);
+likes *= 2; // console.log(likes);
 
+// NaN = Not a number 
+// console.log(  5/ ‘hi’); // output NaN
+//Concatenation string and number 
+console.log(‘You have ’+ likes + ‘ likes on your post’);
+//Template string way 
+const title = ‘Best read of 2020’;
+const author =’Robert’;
+const likes = 30;
+let result = `This blog called ${title} by ${author} has ${likes} likes`;
+console.log(result);
+// creating HTML template
+let html = `<h2>${title}</h2><p>${author}</p>`;
+console.log(html);
 
-let replaceString = email.replace(‘m’,’w’); //  .replace(characterToFind, characterToReplaceWith)
 
 
