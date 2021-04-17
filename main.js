07afd3f61fe8acdd5654cc14dfb53aa4768cc111
infomasudcse <infomasud@gmail.com>
2021-04-18 02:39:09 +0600

    Math operator

--- main.js
+++ main.js
@@ -1,27 +1,45 @@
 // object 
 
-let user = {
-    name : 'Masud',
-    email : 'infomasud@gmail.com',
-    location : 'Dhaka',
-    blogs : ['lorem ispam', 'bello bla'],
-    login : function(){
-        console.log(' the user logged in');
-    },
-    logout: function(){
-        console.log('the user logged out ');
-    },
-    logBlogs: function(){
+// let user = {
+//     name : 'Masud',
+//     email : 'infomasud@gmail.com',
+//     location : 'Dhaka',
+//     blogs : ['lorem ispam', 'bello bla'],
+//     login : function(){
+//         console.log(' the user logged in');
+//     },
+//     logout: function(){
+//         console.log('the user logged out ');
+//     },
+//     logBlogs: function(){
 
-        console.log('the user has following blogs');
-        this.blogs.forEach(blog=>{
-            console.log(blog);
-        })
-    }
+//         console.log('the user has following blogs');
+//         this.blogs.forEach(blog=>{
+//             console.log(blog);
+//         })
+//     }
 
-}
+// }
 
-console.log(user.name);
+// console.log(user.name);
 
 
-user.logBlogs();
+// user.logBlogs();
+
+//Math operator
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+let area = 7.5;
+
+console.log(Math.round(area));
+
+console.log(Math.floor(area));
+
+console.log(Math.ceil(area));
+
+console.log(Math.trunc(area));
+
+console.log(Math.random);
